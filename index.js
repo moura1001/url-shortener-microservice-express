5ed82772b6f45c458e88dbf47d984948408b3ad0
moura1001 <gmouraneto01@gmail.com>
2024-01-29 18:55:29 +0000

    feat: add endpoint for redirection to original URL

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const urls = new Map();
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -17,6 +21,14 @@ app.get("/", function (req, res) {
 // Your first API endpoint
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
+});
+
+app.get("/api/shorturl/:short_url", function (req, res) {
+  if (urls.has(req.params.short_url)) {
+    res.redirect(urls.get(req.params.short_url));
+  } else {
+    res.json({ error: "invalid url: shor_url does not exist" });
+  }
 });
 
 app.listen(port, function () {
