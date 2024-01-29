847a57df23de4678d7c047c5a2bc278a308b6d0b
moura1001 <gmouraneto01@gmail.com>
2024-01-29 19:50:30 +0000

    feat: add endpoint to handle user input url and its respective response with shortened version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -23,11 +24,40 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.post("/api/shorturl", function (req, res) {
+  let url;
+  try {
+    url = new URL(req.body.url);
+  } catch (err) {
+    res.json({ error: "invalid url" });
+    return;
+  }
+
+  for (let [key, value] of urls) {
+    if (value === url.href) {
+      res.json({ original_url: value, short_url: key });
+      return;
+    }
+  }
+
+  dns.lookup(url.hostname, function (err, address, family) {
+    if (err) {
+      res.json({ error: "invalid url" });
+      return;
+    }
+
+    const shortUrl = urls.size + 1;
+    urls.set(shortUrl, url.href);
+    res.json({ original_url: url.href, short_url: shortUrl });
+  });
+});
+
 app.get("/api/shorturl/:short_url", function (req, res) {
-  if (urls.has(req.params.short_url)) {
-    res.redirect(urls.get(req.params.short_url));
+  const shortUrl = req.params.short_url;
+  if (/^\d+$/.test(shortUrl) && urls.has(Number(shortUrl))) {
+    res.redirect(urls.get(Number(shortUrl)));
   } else {
-    res.json({ error: "invalid url: shor_url does not exist" });
+    res.json({ error: "invalid url: short_url does not exist" });
   }
 });
 
